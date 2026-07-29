d28bb3bc0bc8cead19fee51a1db2b5be453903c1
Alberto M <albertom@email.com>
2026-07-29 13:27:23 +0200

    Primeros Commit

---
+++ src/main.py
@@ -0,0 +1,13 @@
+class NormalColumn:
+    def __init__(self, mu, sigma2):
+        self.mu = mu 
+        self.sigma2 = sigma2
+
+print("Hola, España")
+
+class Column:
+    def __init__(self, params):
+        self.params = params
+
+def main():
+    dac = 0
