51f1c50b56967b4f12da4bcefe943ca93ba7fcbd
Alberto M <albertom@email.com>
2026-07-31 00:48:18 +0200

    Desarrollando evaluacion de parametros

--- src/csvator/main.py
+++ src/csvator/main.py
@@ -4,22 +4,23 @@ from simpleeval import simple_eval
 import networkx as nx
 
 class Columna:
-    def __init__(self, name, N, distribution, schema=None, params={}):
-        self.schema = schema
+    def __init__(self, name, N, distribution, params):
         self.name = name
         self.N = N # número de observaciones a generar
         self.distribution = distribution # "normal", "catergorical", "discrete"
         self.params = params
 
-    def set_params(self, params):
-        self.params = params
+    def generate(self):
+        match self.distribution:
+            case "normal":
+                return self.params["mean"] + np.sqrt(self.params["var"])*np.random.randn(self.N)
+            case "discrete":
+                return np.random.choice(self.params["states"], size=self.N, p=self.params["p"])
+            case "categorical":
+                return np.random.binomial(1, self.params["p"], size=self.N)
 
-    def _generate(self, df, N): # Requires all dependencies to be already sampled
-        df_temp = pd.DataFrame()
-        for parameter in self.params:
-            df_temp[parameter] = simple_eval(self.schema, names={column: df[column]})
 
-        df[self.name] = 0
+
 
 class DAG:
     def __init__(self, schema, N):
@@ -31,4 +32,5 @@ class DAG:
         self.columnas = nx.topological_sort(self.graph) # Perfectly sorted!
 
     def generate(self, columna):
-        pass
+        for columna in self.columnas:
+            params = 0
